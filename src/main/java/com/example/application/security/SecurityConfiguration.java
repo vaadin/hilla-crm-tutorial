c53ad1c514c746f0d49804dab8eb0f452d6cbdf0
Marcus Hellberg <marcus@vaadin.com>
2021-10-19 11:39:55 -0700

    stateless auth

--- src/main/java/com/example/application/security/SecurityConfiguration.java
+++ src/main/java/com/example/application/security/SecurityConfiguration.java
@@ -1,20 +1,31 @@
 package com.example.application.security;
 
+import java.util.Base64;
+
+import javax.crypto.spec.SecretKeySpec;
+
 import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jose.jws.JwsAlgorithms;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {
 
+  @Value("${jwt.key}")
+  private String JWTKey;
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     super.configure(http);
     setLoginView(http, "/login");
+    setStatelessAuthentication(http, new SecretKeySpec(Base64.getDecoder().decode(JWTKey), JwsAlgorithms.HS256),
+        "com.example.application");
   }
 
   @Override
