c4928dc856241538565a5ae51e4f1d211d8e20ef
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2023-10-27 15:26:15 +0300

    Listing Contacts in a Data Grid

--- frontend/views/contacts/ContactsView.tsx
+++ frontend/views/contacts/ContactsView.tsx
@@ -1,7 +1,29 @@
+import ContactRecord from 'Frontend/generated/com/example/application/services/CRMService/ContactRecord';
+import {useEffect, useState} from 'react';
+import {CRMService} from "Frontend/generated/endpoints";
+import {Grid} from "@hilla/react-components/Grid";
+import {GridColumn} from "@hilla/react-components/GridColumn";
+
 export default function ContactsView() {
-  return (
-    <div className="p-m">
-      <h2>Contacts</h2>
-    </div>
-  );
+    const [contacts, setContacts] = useState<ContactRecord[]>([]);
+    const [selected, setSelected] = useState<ContactRecord | null | undefined>();
+
+    useEffect(() => {
+        CRMService.findAllContacts().then(setContacts);
+    }, []);
+
+    return (
+        <div className="p-m flex gap-m">
+            <Grid
+                items={contacts}
+                onActiveItemChanged={e => setSelected(e.detail.value)}
+                selectedItems={[selected]}>
+
+                <GridColumn path="firstName"/>
+                <GridColumn path="lastName"/>
+                <GridColumn path="email" autoWidth/>
+                <GridColumn path="company.name" header="Company name"/>
+            </Grid>
+        </div>
+    );
 }
